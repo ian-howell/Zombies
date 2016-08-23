6f4222406480b7979df622cbef643b92a875a9c3
Ian Howell <ian.howell0@gmail.com>
2016-08-22 19:54:37 -0500

    Draw something

--- main.cpp
+++ main.cpp
@@ -9,7 +9,18 @@ int main()
     if (sys.init("Zombies!") != 0)
         return 1;
 
-    while (!keypressed()) {}
+    int x = (WIDTH / 2) - (WIDTH / 4);
+    int dir = 1;
+    while (!keypressed())
+    {
+        clear_to_color(screen, makecol(0, 0, 0));
+        circle(screen, x, HEIGHT / 2, 20, makecol(255, 0, 0));
+
+        if (x + dir > 0.75 * WIDTH || x + dir < 0.25 * WIDTH)
+            dir *= -1;
+
+        x += dir;
+    }
 
     return 0;
 }
